f4ad719c5f53efbfe0115a364a0524dc3a77242a
Ashish Papanai <ashishpapanai00@gmail.com>
2021-02-25 18:49:42 +0530

    Ready for pip package

--- app/forms.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
-from wtforms import StringField, TextAreaField, SubmitField
-from wtforms.validators import DataRequired, Length
-
-class LoginForm(FlaskForm):
-    username = StringField('Ticker', validators=[DataRequired()])
-    submit = SubmitField('Get Predictions')
