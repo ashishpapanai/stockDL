95b15f7a7313c4c62892c5635c0712acdb91776c
Ashish Papanai <52123364+ashishpapanai@users.noreply.github.com>
2021-02-27 19:45:02 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="stockDL",
-    version="0.0.2",
+    version="0.0.3",
     description="Predicts the Gross Yield, Annual Yield and Net Yield of a user given stock ticker.",
     long_description=README,
     long_description_content_type="text/markdown",
